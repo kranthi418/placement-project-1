db1c26237d7b04e9a398375035c2a2bc45e00081
kranthi418 <kranthinaik09@gmail.com>
2025-12-07 22:11:06 +0530

    completed modules

--- upload_data.py
+++ upload_data.py
@@ -5,7 +5,7 @@ url="mongodb+srv://kranthi:NwtPxSWouijDe4hp@cluster0.dqik7.mongodb.net/?appName=
 client = MongoClient(url)
 DATABASE_NAME="Venkat"
 COLLECTION_NAME="waferfault"
-df=pd.read_csv("C:\Users\kranthi-418\OneDrive\Desktop\placement project 1\notebooks\wafer_23012020_041211.csv")
+df=pd.read_csv("C:\Users\kranthi-418\OneDrive\Desktop\placement project 1\artifacts\wafer_23012020_041211.csv")
 df=df.drop("Unnamed: 0",axis=1)
 json_record=list(json.loads(df.T.to_json()).values())
 client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
